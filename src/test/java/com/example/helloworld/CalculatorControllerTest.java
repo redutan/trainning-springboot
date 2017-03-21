a8beac83d53ad800963b86fba23da78becaa99a5
myeongju.jung <redutan@gmail.com>
2017-03-21 17:19:32 +0900

    Set<Integer> numbers 확인

--- src/test/java/com/example/helloworld/CalculatorControllerTest.java
+++ src/test/java/com/example/helloworld/CalculatorControllerTest.java
@@ -4,20 +4,15 @@ import com.example.ControllerTestSupport;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.joining;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -141,5 +136,17 @@ public class CalculatorControllerTest extends ControllerTestSupport {
         ResultActions ra = mockMvc.perform(get("/calculator/adder2/"));
         // Then
         ra.andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testAdder3_121() throws Exception {
+        // Given
+        final List<Integer> numbers = Arrays.asList(1, 2, 1);
+        final String numbersString = numbers.stream().map(String::valueOf).collect(joining(","));
+        // When
+        ResultActions ra = mockMvc.perform(get("/calculator/adder3/{numbers}", numbersString));
+        // Then
+        ra.andExpect(status().isOk());  // !!!
+        ra.andExpect(content().string(String.valueOf(3)));
     }
 }
