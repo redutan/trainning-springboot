ecfaf91575df4de95935cafae7c750f4f5bb9712
myeongju.jung <redutan@gmail.com>
2017-04-02 15:53:33 +0900

    검색 뷰 수정

--- src/main/java/com/example/board/dto/BoardSearch.java
+++ src/main/java/com/example/board/dto/BoardSearch.java
@@ -4,7 +4,6 @@ import com.example.board.Board;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 
@@ -18,22 +17,29 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Slf4j
 public class BoardSearch {
+    private static final Predicate[] EMPTY_PREDICATES = new Predicate[0];
+    /**
+     * 제목 검색
+     */
     private String title;
+    /**
+     * 작성자 검색
+     */
     private String writer;
 
     public Specification<Board> toSpec() {
         return (root, query, builder) -> {
             List<Predicate> predicates = new ArrayList<>();
-
+            // 제목 contains 검색
             if (!StringUtils.isEmpty(title)) {
                 predicates.add(builder.like(root.get("title"), "%" + title + "%"));
             }
+            // 작성자 equal 검색
             if (!StringUtils.isEmpty(writer)) {
                 predicates.add(builder.equal(root.get("writer"), writer));
             }
-            return builder.and(predicates.stream().toArray(Predicate[]::new));
+            return builder.and(predicates.toArray(EMPTY_PREDICATES));
         };
     }
 }
