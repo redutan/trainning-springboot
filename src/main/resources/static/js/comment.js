df71c06c9f19a933cc7c4fb569426b2a51c13249
myeongju.jung <redutan@gmail.com>
2017-04-10 01:48:04 +0900

    - 댓글 삭제 구현 - js 분리

---
+++ src/main/resources/static/js/comment.js
@@ -0,0 +1,140 @@
+var Comments = React.createClass({
+    loadComponentFromServer: function() {
+        $.ajax({
+            url: "/boards/" + this.props.boardSeq + "/comments"
+            ,dataType: "json"
+            ,cache: false
+            ,success: function(comments) {
+                this.setState({data: comments});
+            }.bind(this)
+            ,error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleDeleteComment: function(comment) {
+        if (!confirm("삭제하시겠습니까?")) {
+            return;
+        }
+        $.ajax({
+            url: "/boards/" + this.props.boardSeq + "/comments" + "/" + comment.seq
+            ,type: "delete"
+            ,dataType: "json"
+            ,cache: false
+            ,success: function() {
+                var comments = this.state.data;
+                comments.splice(comments.indexOf(comment), 1);
+                this.setState({data: comments});
+            }.bind(this)
+            ,error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleCreateComment: function(comment) {
+        $.ajax({
+            url: "/boards/" + this.props.boardSeq + "/comments"
+            ,type: "post"
+            ,dataType: "json"
+            ,contentType: "application/json"
+            ,data: JSON.stringify(comment)
+            ,cache: false
+            ,success: function(comment) {
+                var comments = this.state.data;
+                var newComments = comments.concat([comment]);
+                this.setState({data: newComments});
+            }.bind(this)
+            ,error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function () {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.loadComponentFromServer();
+    },
+    render: function() {
+        var handleDeleteComment = this.handleDeleteComment;
+        var comments = this.state.data.map(function (comment) {
+            return (
+                <Comment comment={comment} key={comment.seq} onDeleteComment={handleDeleteComment}/>
+            );
+        });
+        return (
+            <div className="container">
+                {comments}
+                <CommentForm onCreateComment={this.handleCreateComment}/>
+            </div>
+        );
+    }
+});
+var Comment = React.createClass({
+    dateFormat: function (timeMillis) {
+        var date = new Date(timeMillis);
+        return date.dateFormat('yyyy.mm.dd hh:ii:ss');
+    },
+    handleDelete: function(e) {
+        e.preventDefault();
+        this.props.onDeleteComment(this.props.comment);
+    },
+    render: function () {
+        return (
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    {this.dateFormat(this.props.comment.regDate)} &nbsp; {this.props.comment.writer} &nbsp;
+                    <button className="btn btn-danger btn-xs" onClick={this.handleDelete}>x</button>
+                </div>
+                <div className="panel-body">
+                    {this.props.comment.content}
+                </div>
+            </div>
+        );
+    }
+});
+var CommentForm = React.createClass({
+    handleCreate: function(e) {
+        var getValueAndCheck = function(ref, msg) {
+            var element = ReactDOM.findDOMNode(ref);
+            var value = element.value.trim();
+            if (value === "") {
+                alert(msg);
+                element.focus();
+            }
+            return value;
+        };
+        var setEmptyValue = function(ref) {
+            ReactDOM.findDOMNode(ref).value = "";
+        };
+
+        e.preventDefault();
+        var writer = getValueAndCheck(this.refs.writer, "작성자를 입력해주세요.");
+        var content = getValueAndCheck(this.refs.content, "내용을 입력해주세요.");
+        this.props.onCreateComment({writer: writer, content: content});
+        setEmptyValue(this.refs.writer);
+        setEmptyValue(this.refs.content);
+        return;
+    },
+    render: function() {
+        return (
+            <form onSubmit={this.handleCreate}>
+                <div className="panel panel-info">
+                    <div className="panel-heading">
+                        <input type="text" name="writer" className="form-control" ref="writer" placeholder="작성자"/>
+                    </div>
+                    <div className="panel-body">
+                        <textarea className="form-control" name="content" ref="content" placeholder="댓글내용"></textarea>
+                        <div className="btn-group btn-group-sm pull-right" role="group">
+                            <button type="submit" className="btn btn-primary">등록</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+});
+ReactDOM.render(
+    <Comments boardSeq={window._initParam.boardSeq}/>,
+    document.getElementById("comments")
+);
---
+++ src/main/resources/static/js/date.js
@@ -0,0 +1,15 @@
+Date.prototype.dateFormat = function(format) {
+    var yyyy = this.getFullYear().toString();
+    format = format.replace(/yyyy/g, yyyy)
+    var mm = (this.getMonth()+1).toString();
+    format = format.replace(/mm/g, (mm[1]?mm:"0"+mm[0]));
+    var dd  = this.getDate().toString();
+    format = format.replace(/dd/g, (dd[1]?dd:"0"+dd[0]));
+    var hh = this.getHours().toString();
+    format = format.replace(/hh/g, (hh[1]?hh:"0"+hh[0]));
+    var ii = this.getMinutes().toString();
+    format = format.replace(/ii/g, (ii[1]?ii:"0"+ii[0]));
+    var ss  = this.getSeconds().toString();
+    format = format.replace(/ss/g, (ss[1]?ss:"0"+ss[0]));
+    return format;
+};
