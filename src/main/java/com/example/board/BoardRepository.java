75ee08edbab991161a08c555850454c8024c252d
myeongju.jung <redutan@gmail.com>
2017-04-24 22:22:24 +0900

    - spring-data-rest 적용

--- src/main/java/com/example/board/BoardRepository.java
+++ src/main/java/com/example/board/BoardRepository.java
@@ -2,7 +2,6 @@ package com.example.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -10,7 +9,6 @@ import java.util.List;
  * 게시물 저장소
  */
 @SuppressWarnings("WeakerAccess")
-@Repository
 public interface BoardRepository extends JpaRepository<Board, Long>, JpaSpecificationExecutor<Board> {
     List<Board> findByTitleContaining(String title);
 
