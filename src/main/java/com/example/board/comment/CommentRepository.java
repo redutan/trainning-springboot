c27e454373a00552bb20f1beb104151854dd81af
myeongju.jung <redutan@gmail.com>
2017-05-22 22:24:55 +0900

    docker 작성 중

--- src/main/java/com/example/board/comment/CommentRepository.java
+++ src/main/java/com/example/board/comment/CommentRepository.java
@@ -4,13 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 //@Repository
-@PreAuthorize("hasRole('USER')")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Override
-    @PreAuthorize("@commentRepository.findOne(#id)?.writer == authentication?.name")
+    @PreAuthorize("hasRole('USER') and @commentRepository.findOne(#id)?.writer == authentication?.name")
     void delete(Long id);
 
     @Override
-    @PreAuthorize("#comment?.writer == authentication?.name")
+    @PreAuthorize("hasRole('USER') and #comment?.writer == authentication?.name")
     void delete(Comment comment);
 }
