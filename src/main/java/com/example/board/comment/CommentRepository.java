740967bceac3b6f6020d163c9bb718f3af6c193c
myeongju.jung <redutan@gmail.com>
2017-05-22 20:35:35 +0900

    Comment 테스트 검증

--- src/main/java/com/example/board/comment/CommentRepository.java
+++ src/main/java/com/example/board/comment/CommentRepository.java
@@ -1,7 +1,16 @@
 package com.example.board.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 //@Repository
+@PreAuthorize("hasRole('USER')")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Override
+    @PreAuthorize("@commentRepository.findOne(#id)?.writer == authentication?.name")
+    void delete(Long id);
+
+    @Override
+    @PreAuthorize("#comment?.writer == authentication?.name")
+    void delete(Comment comment);
 }
--- src/test/java/com/example/board/comment/CommentControllerIntegrate1Test.java
+++ src/test/java/com/example/board/comment/CommentControllerIntegrate1Test.java
@@ -25,30 +25,33 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.function.Supplier;
 
+import static com.example.board.comment.CommentControllerIntegrate1Test.USERNAME;
 import static io.github.benas.randombeans.api.EnhancedRandom.random;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static io.github.benas.randombeans.api.EnhancedRandom.randomListOf;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@WithMockUser(username = "user", roles = "USER")
+@WithMockUser(username = USERNAME, roles = "USER")
 @Slf4j
 public class CommentControllerIntegrate1Test {
+    public static final String USERNAME = "user";
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
     @Autowired
     private BoardRepository boardRepository;
+    @Autowired
+    private CommentRepository commentRepository;
     @Qualifier("halJacksonHttpMessageConverter")
     @Autowired
     private TypeConstrainedMappingJackson2HttpMessageConverter halJacksonHttpMessageConverter;
@@ -105,5 +108,66 @@ public class CommentControllerIntegrate1Test {
                 ResolvableType.forClassWithGenerics(Resource.class, responseClass).getType(),
                 null,
                 new MockHttpInputMessage(mvcResult.getResponse().getContentAsByteArray()));
+    }
+
+    @Test
+    public void testList() throws Exception {
+        // Given
+        saveComments(11);
+        // When
+        mockMvc.perform(get("/api/comments")
+                .with(csrf())
+                .accept(MediaTypes.HAL_JSON)
+                .contentType(MediaTypes.HAL_JSON))
+                // Then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.comments", hasSize(11)));
+    }
+
+    private List<Comment> saveComments(int size) {
+        return saveComments(size, null);
+    }
+
+    private List<Comment> saveComments(int size, Supplier<String> writerSupplier) {
+        List<Comment> willSaves = randomListOf(size, Comment.class, "seq", "board");
+        for (Comment each : willSaves) {
+            each.setBoard(board);
+            if (writerSupplier != null) {
+                each.setWriter(writerSupplier.get());
+            }
+        }
+        return commentRepository.save(willSaves);
+    }
+
+    @Test
+    public void testDelete_OtherUser() throws Exception {
+        // Given
+        List<Comment> saves = saveComments(1);
+        Comment willDelete = saves.get(0);
+        // When
+        mockMvc.perform(delete("/api/comments/{commentSeq}", willDelete.getSeq())
+                .with(csrf())
+                .accept(MediaTypes.HAL_JSON)
+                .contentType(MediaTypes.HAL_JSON))
+                // Then
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        // Given
+        List<Comment> saves = saveMyComments(1);
+        Comment willDelete = saves.get(0);
+        // When
+        mockMvc.perform(delete("/api/comments/{commentSeq}", willDelete.getSeq())
+                .with(csrf())
+                .accept(MediaTypes.HAL_JSON)
+                .contentType(MediaTypes.HAL_JSON))
+                // Then
+                .andExpect(status().isNoContent());
+    }
+
+    private List<Comment> saveMyComments(@SuppressWarnings("SameParameterValue") int size) {
+        return saveComments(size, () -> USERNAME);
     }
 }
